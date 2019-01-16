35404b820e8b0e6386db86b8d7c9f3549953630f
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-01-16 16:32:29 +0300

    Сделаны задания 3-8

--- lesson1/script.js
+++ lesson1/script.js
@@ -5,16 +5,16 @@
  * где Tf –температура по Фаренгейту, Tc – температура по Цельсию
  */
 function converter() {
-    var tempCelcium = prompt("Введите температуру в Цельсиях", 36.6); 
-    // Вызываем alert и повторение ввода в случае, если не было нажато 'Отмена'
-    if (isNaN(tempCelcium)) {
-      alert('Нужно ввести число');
-    } else {
-      if (!(tempCelcium === null)) {
-        alert((9 / 5) * tempCelcium + 32);
-        converter();
-      }
+  var tempCelcium = prompt("Введите температуру в Цельсиях", 36.6);
+  // Вызываем alert и повторение ввода в случае, если не было нажато 'Отмена'
+  if (isNaN(tempCelcium)) {
+    alert('Нужно ввести число');
+  } else {
+    if (!(tempCelcium === null)) {
+      alert((9 / 5) * tempCelcium + 32);
+      converter();
     }
+  }
 }
 
 /**
@@ -24,11 +24,11 @@ function converter() {
  * Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
  */
 function admin() {
-    var admin;
-    var name;
-    name = "Василий";
-    admin = name;
-    alert(admin);
+  var admin;
+  var name;
+  name = "Василий";
+  admin = name;
+  alert(admin);
 }
 
 /**
@@ -36,10 +36,10 @@ function admin() {
  * Чему будет равно JS-выражение 1000 + "108"
  */
 function ex3() {
-    var element = document.getElementById("answer3");
-    element.innerHTML = "JS-выражение 1000 + \"108\" будет равняться  <b>\"1000108\"</b>. Это связано с тем, что JS приводит " +
-        "number 1000 к типу string, а для этого типа оператор \"+\" выполняет конкатенацию.";
-    setStyle(element);
+  var element = document.getElementById("answer3");
+  element.innerHTML = "JS-выражение 1000 + \"108\" будет равняться  <b>\"1000108\"</b>. Это связано с тем, что JS приводит " +
+    "number 1000 к типу string, а для этого типа оператор \"+\" выполняет конкатенацию.";
+  setStyle(element);
 }
 
 /**
@@ -47,17 +47,17 @@ function ex3() {
  * Самостоятельно разобраться с атрибутами тега script (async и defer)
  */
 function ex4() {
-    var element = document.getElementById("answer4");
-    element.innerHTML = "<p><b>async</b> - cкрипт выполняется полностью асинхронно. Браузер не останавливает обработку " +
-        "страницы, а спокойно работает дальше. Когда скрипт будет загружен – он выполнится.</p>" +
-        "<p><b>defer</b> - cкрипт также выполняется асинхронно. Но есть отличия: defer гарантирует " +
-        "последовательное выполнение скриптов.</p>";
-    setStyle(element);
+  var element = document.getElementById("answer4");
+  element.innerHTML = "<p><b>async</b> - cкрипт выполняется полностью асинхронно. Браузер не останавливает обработку " +
+    "страницы, а спокойно работает дальше. Когда скрипт будет загружен – он выполнится.</p>" +
+    "<p><b>defer</b> - cкрипт также выполняется асинхронно. Но есть отличия: defer гарантирует " +
+    "последовательное выполнение скриптов.</p>";
+  setStyle(element);
 }
 
 function setStyle(element) {
-    element.style.padding = "5px";
-    element.style.border = "2px solid gray";
-    element.style.borderRadius = "5px";
-    element.style.marginTop = "10px";
+  element.style.padding = "5px";
+  element.style.border = "2px solid gray";
+  element.style.borderRadius = "5px";
+  element.style.marginTop = "10px";
 }
--- lesson2/script.js
+++ lesson2/script.js
@@ -1,163 +1,200 @@
-    /*
-    Задание 1
-    */
-    function ex1() {
-      var a = 1,
-      b = 1,
-      c, d;
-    c = ++a;
-    alert("++a = " + c + " - 1+1 сначало прибавили, а потом вывели");
-    d = b++;
-    alert("b++ = " + d + " - 1 сначало вывели, а потом прибавили");
-    c = (2 + ++a);
-    alert("2 + (++a) = " + c + " - т.к. а = 2, мы сначало прибовляем 1 выполняя операцию ++а, потом прибовляем 2");
-    d = (2 + b++);
-    alert("2 + (b++) = " + d + " - т.к. b уже = 2 (то предыдущего b++) и мы прибовляем еще 2, и запоминаем что в следующем выводе будет +1");
-    alert("a = " + a + " -  мы выполнили 2 действия ++а");
-    alert("b = " + b + " - мы выполнили 2 действия b++ и вывели его 3 раза, что позволило прибавиться еще 1");
-    }
-    
-    /*
-    Задание 2
-    */
-    function ex2() {
-      var a = 2;
-    var x = 1 + (a *= 2);
-    alert("a = a * 2 = " + a + " - мы присваиваем и умнажаем на 2 переменную а");
-    alert("1 + (a * 2) = " + x + " - т.к. у нас сначало выполняются действия в () у нас а = 4 и мы прибовляем 1"); 
-    }
-    
-    /*
-    Задание 3
-    */
-    function ex3() {
-      var variable_1 = prompt("Введите число: ", -5);
-    var variable_2 = prompt("Введите число: ", 10);
-    if (variable_1 >= 0 && variable_2 >= 0) {
-      alert('Введенные числа положительные: ' + (variable_1 - variable_2));
-    } else if (variable_1 < 0 && variable_2 < 0) {
-      alert('Введенные числа отрицательные: ' + (variable_1 * variable_2));
-    } else {
-      alert('Числа с разными знаками: ' + (variable_1 + variable_2));
-    }
-    }
-    
-    /*
-    Задание 4
-    */
-    console.log("Задание 4");
-    var a = +prompt('Введите число от 1 до 15');
+/*
+Задание 1
+*/
+function ex1() {
+  var a = 1,
+    b = 1,
+    c, d;
+  c = ++a;
+  alert("++a = " + c + " - 1+1 сначало прибавили, а потом вывели");
+  d = b++;
+  alert("b++ = " + d + " - 1 сначало вывели, а потом прибавили");
+  c = (2 + ++a);
+  alert("2 + (++a) = " + c + " - т.к. а = 2, мы сначало прибовляем 1 выполняя операцию ++а, потом прибовляем 2");
+  d = (2 + b++);
+  alert("2 + (b++) = " + d + " - т.к. b уже = 2 (то предыдущего b++) и мы прибовляем еще 2, и запоминаем что в следующем выводе будет +1");
+  alert("a = " + a + " -  мы выполнили 2 действия ++а");
+  alert("b = " + b + " - мы выполнили 2 действия b++ и вывели его 3 раза, что позволило прибавиться еще 1");
+}
+
+/*
+Задание 2
+*/
+function ex2() {
+  var a = 2;
+  var x = 1 + (a *= 2);
+  alert("a = a * 2 = " + a + " - мы присваиваем и умнажаем на 2 переменную а");
+  alert("1 + (a * 2) = " + x + " - т.к. у нас сначало выполняются действия в () у нас а = 4 и мы прибовляем 1");
+}
+
+/*
+Задание 3
+*/
+function ex3() {
+  var variable_1 = prompt("Введите число: ", -5);
+  var variable_2 = prompt("Введите число: ", 10);
+  if (isNaN(variable_1) || isNaN(variable_2) || variable_1 == null || variable_2 == null) {
+    alert('Нужно ввести число');
+  } else if (variable_1 > 0 && variable_2 > 0) {
+    alert('Введенные числа положительные: ' + (variable_1 - variable_2));
+  } else if (variable_1 < 0 && variable_2 < 0) {
+    alert('Введенные числа отрицательные: ' + (variable_1 * variable_2));
+  } else if (variable_1 < 0 && variable_2 > 0 || variable_1 > 0 && variable_2 < 0) {
+    var x = Number(variable_1) + Number(variable_2);
+    alert('Числа с разными знаками: ' + x);
+  } else {
+    alert('Нужно ввести число');
+  }
+}
+
+/*
+Задание 4
+*/
+function ex4() {
+  var a = +prompt('Введите число от 1 до 15');
+  if (isNaN(a) || a === null) {
+    alert('Нужно ввести число');
+  } else {
     switch (a) {
       case 1:
-        console.log('1');
+        alert("1");
       case 2:
-        console.log('2');
+        alert("2");
       case 3:
-        console.log('3');
+        alert("3");
       case 4:
-        console.log('4');
+        alert("4");
       case 5:
-        console.log('5');
+        alert("5");
       case 6:
-        console.log('6');
+        alert("6");
       case 7:
-        console.log('7');
+        alert("7");
       case 8:
-        console.log('8');
+        alert("8");
       case 9:
-        console.log('9');
+        alert("9");
       case 10:
-        console.log('10');
+        alert("10");
       case 11:
-        console.log('11');
+        alert("11");
       case 12:
-        console.log('12');
+        alert("12");
       case 13:
-        console.log('13');
+        alert("13");
       case 14:
-        console.log('14');
+        alert("14");
       case 15:
-        console.log('15');
+        alert("15");
         break;
     }
-    /*
-    Задание 5
-    */
-    console.log("Задание 5");
+  }
+}
+/*
+Задание 5
+*/
+function plus(a, b) {
+  return Number(a) + Number(b);
+}
 
-    function plus(a, b) {
-      return a + b;
-    }
+function minus(a, b) {
+  return a - b;
+}
 
-    function minus(a, b) {
-      return a - b;
-    }
+function div(a, b) {
+  return a / b;
+}
 
-    function div(a, b) {
-      return a / b;
-    }
+function mult(a, b) {
+  return a * b;
+}
 
-    function mult(a, b) {
-      return a * b;
-    }
-    console.log('Сложение ', plus(5, 9));
-    console.log('Вычитание ', minus(5, 9));
-    console.log('Деление ', div(5, 9));
-    console.log('Произведение ', mult(5, 9));
-    /*
-    Задание 6
-    */
-    console.log("Задание 6");
+function ex5() {
+  var addend1 = prompt('Введите первое слагаемое');
+  var addend2 = prompt('Введите второе слагаемое');
+  if (isNaN(addend1) || addend1 === null && isNaN(addend2) || addend2 === null) {
+    alert('Нужно ввести число');
+  } else {
+    alert('Сложение ' + plus(addend1, addend2));
+    alert('Вычитание ' + minus(addend1, addend2));
+    alert('Деление ' + div(addend1, addend2));
+    alert('Произведение ' + mult(addend1, addend2));
+  }
+}
+/*
+Задание 6
+*/
+function mathOperation(arg1, arg2, operation) {
+  switch (operation) {
+    case '+':
+      return plus(arg1, arg2);
+      break;
+    case '-':
+      return minus(arg1, arg2);
+      break;
+    case '/':
+      return div(arg1, arg2);
+      break;
+    case '*':
+      return mult(arg1, arg2);
+      break;
+  }
+}
 
-    function mathOperation(arg1, arg2, operation) {
-      switch (operation) {
-        case 'сложение':
-          return plus(arg1, arg2);
-          break;
-        case 'вычитание':
-          return minus(arg1, arg2);
-          break;
-        case 'деление':
-          return div(arg1, arg2);
-          break;
-        case 'умножение':
-          return mult(arg1, arg2);
-          break;
-      }
-    }
-    console.log('Сложение ', mathOperation(6, 9, 'сложение'));
-    console.log('Вычитание ', mathOperation(5, 6, 'вычитание'));
-    console.log('Деление ', mathOperation(6, 9, 'деление'));
-    console.log('Произведение ', mathOperation(5, 6, 'умножение'));
+function ex6() {
+  var addend1 = prompt('Введите первое слагаемое');
+  var addend2 = prompt('Введите второе слагаемое');
+  var symbol = prompt('Введите математический символ: + - / * ');
+  if (isNaN(addend1) || addend1 === null && isNaN(addend2) || addend2 === null) {
+    alert('Нужно ввести число');
+  } else if (symbol === '+') {
+    alert('Сложение ' + mathOperation(addend1, addend2, symbol));
+  } else if (symbol === '-') {
+    alert('Вычитание ' + mathOperation(addend1, addend2, symbol));
+  } else if (symbol === '/') {
+    alert('Деление ' + mathOperation(addend1, addend2, symbol));
+  } else if (symbol === '*') {
+    alert('Произведение ' + mathOperation(addend1, addend2, symbol));
+  } else {
+    alert('Нужно ввести математический символ: + - / *');
+  }
+}
 
-    /*
-    Задание 7
-    */
-    console.log("Задание 7");
-    var ziro = 0,
-      nul = null;
-    console.log("Типы данных 0 и null: " + typeof (ziro) + " и " + typeof (nul));
-    console.log("0 + null: " + (ziro + nul));
-    console.log("0 - null: " + (ziro - nul));
-    console.log("0 * null: " + (ziro * nul));
-    console.log("0 / null: " + (ziro / nul));
-    console.log(ziro + " - это число 0");
-    console.log(nul + " - это пустая ячейка памяти");
-
-    /*
-    Задание 8
-    */
-    console.log("Задание 8");
-    // a^n = a*a*a*a*a*...*a;
-    // val – заданное число 5
-    // pow – степень.       3
-    function power(vol, pow) {
-      var x = (pow != 1) ? (vol * power(vol, pow - 1)) : vol;
-      return x;
-      //      if (pow != 1) {
-      //        return vol * power(vol, pow - 1);
-      //      } else {
-      //        return vol;
-      //      }
-    }
-    console.log(power(5, 4));
+/*
+Задание 7
+*/
+function ex7() {
+  var ziro = 0,
+    nul = null;
+  alert("Типы данных 0 и null: " + typeof (ziro) + " и " + typeof (nul));
+  alert("0 + null: " + (ziro + nul));
+  alert("0 - null: " + (ziro - nul));
+  alert("0 * null: " + (ziro * nul));
+  alert("0 / null: " + (ziro / nul));
+  alert(ziro + " - это число 0");
+  alert(nul + " - это пустая ячейка памяти");
+}
+/*
+Задание 8
+*/
+// a^n = a*a*a*a*a*...*a;
+// val – заданное число 5
+// pow – степень.       3
+function power(vol, pow) {
+  var x = (pow != 1) ? (vol * power(vol, pow - 1)) : vol;
+  return x;
+  //      if (pow != 1) {
+  //        return vol * power(vol, pow - 1);
+  //      } else {
+  //        return vol;
+  //      }
+}
+function ex8() {
+  var addend1 = prompt('Введите число');
+  var addend2 = prompt('Введите степень');
+  if (isNaN(addend1) || addend1 === null && isNaN(addend2) || addend2 === null) {
+    alert('Нужно ввести число');
+  } else {
+    alert(power(addend1, addend2));
+  }
+}
