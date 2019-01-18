a5f4e8dfcf22d9146586d98b3e1ffa11f3804e09
VasilyAK <kaluginchernyhovsck@mail.ru>
2019-01-18 11:19:07 +0200

    Подправил валидацию входящих параметров в доп задании

--- lesson2/Дополнительное задание/add_task_1/01.js
+++ lesson2/Дополнительное задание/add_task_1/01.js
@@ -5,22 +5,26 @@
 function fn_revers (n){    
     let count = 'Ваши числа:';
     try {    
-        if (typeof n === 'number'){ //проверяем тип параметра
-            if (parseInt(n) === parseFloat(n)){ // число должно быть 'integer'
-                if (n >0 && n < 101){ // число должно быть от 1 до 100
-                    document.write(fn_Numbers(n).split(' ').sort(function(a,b){return a-b;}).join(' ')+'</br></br>'); //произвожу реверс чисел, т.к. по условию числа должны быть от 1 до n+'</br></br>');                            
+        if (arguments.length === 1){    
+            if (typeof n === 'number'){ //проверяем тип параметра
+                if (parseInt(n) === parseFloat(n)){ // число должно быть 'integer'
+                    if (n >0 && n < 101){ // число должно быть от 1 до 100
+                        document.write(fn_Numbers(n).split(' ').sort(function(a,b){return a-b;}).join(' ')+'</br></br>'); //произвожу реверс чисел, т.к. по условию числа должны быть от 1 до n+'</br></br>');                            
+                    } else {
+                        throw new Error('Function parameter must be between 1 and 100');
+                    };
                 } else {
-                    throw new Error('Function parameter must be between 1 and 100');
-                };
+                    throw new Error('Function parameter must be \'integer\'');
+                };                
             } else {
-                throw new Error('Function parameter must be \'integer\'');
-            };                
+                throw new Error('Function parameter must be \'number\'');
+            };
         } else {
-            throw new Error('Function parameter must be \'number\'');
+            throw new Error('Count of function parameters must be 1');
         };
-        } catch (e){
-            console.log(e);
-        };
+    } catch (e){
+        console.log(e);
+    };
     
     function fn_Numbers (n){
         count += ' ' + n;
