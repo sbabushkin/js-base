2f6fda0dae15369922b7b9be57da38f976e64742
Yana <33156136+YanaSezko@users.noreply.github.com>
2019-01-18 14:57:57 +0200

    lesson2

---
+++ lesson2/Lesson2_JS/js/ex4.js
@@ -0,0 +1,39 @@
+//задание 4
+function c() {
+	var a = Math.floor(Math.random() * 16);
+	alert(a);
+	switch (a) {
+		case 0:
+			alert('1');
+		case 1:
+			alert('2');
+		case 2:
+			alert('3');
+		case 3:
+			alert('4');
+		case 4:
+			alert('5');
+		case 5:
+			alert('6');
+		case 6:
+			alert('7');
+		case 7:
+			alert('8');
+		case 8:
+			alert('9');
+		case 9:
+			alert('10');
+		case 10:
+			alert('11');
+		case 11:
+			alert('12');
+		case 12:
+			alert('13');
+		case 13:
+			alert('14');
+		case 14:
+			alert('15');
+		default:
+			alert('Конец');
+	}
+}
