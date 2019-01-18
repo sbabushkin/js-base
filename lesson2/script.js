31ca1de54563783324274c4122bb872db2594451
Anatoly Kulikov <anatoly.andrew.kulikov@yandex.ru>
2019-01-18 17:24:23 +0300

    update function letOutput()
    Для функции для счета до 15 от значения переменной использовалась конструкция switch case, но потом я вспомнил, что есть ещё цикл while, что помогло сократить код на много строк

--- lesson2/script.js
+++ lesson2/script.js
@@ -71,63 +71,17 @@
 
     // Функция для счета до 15 от значения переменной
     function letOutput() {
-        let a = Math.trunc(Math.random() * 15);
-        let b = 0;
+        let a = Math.trunc(Math.random()*15), b = 0;
+         
         console.log('Переменная a = ' + a + '.');
-        switch (a) {
-            case 0:
-                console.log(a++);
-                b++;
-            case 1:
-                console.log(a++);
-                b++;
-            case 2:
-                console.log(a++);
-                b++;
-            case 3:
-                console.log(a++);
-                b++;
-            case 4:
-                console.log(a++);
-                b++;
-            case 5:
-                console.log(a++);
-                b++;
-            case 6:
-                console.log(a++);
-                b++;
-            case 7:
-                console.log(a++);
-                b++;
-            case 8:
-                console.log(a++);
-                b++;
-            case 9:
-                console.log(a++);
-                b++;
-            case 10:
-                console.log(a++);
-                b++;
-            case 11:
-                console.log(a++);
-                b++;
-            case 12:
-                console.log(a++);
-                b++;
-            case 13:
-                console.log(a++);
-                b++;
-            case 14:
-                console.log(a++);
-                b++;
-            case 15:
-                console.log(a++);
-                b++;
-                break;
+        
+        while (a <= 15) {
+            console.log(a++);
+            b++;
         }
         console.log('Счет закончен, сделано ' + b + ' шагов.');
-        // ИМХО, тут мне видится быдлокодинг в своем исполнении, чувствую, что можно как-то более элегантно и быстро это сделать
-    }
+        // Тут был вывод с помощью switch case, но потом я вспомнил, что есть ещё цикл while, что помогло сократить код на много строк
+     }
 
     // Функция математической операции в зависимости от значений чисел
     function firstMathoper() {
