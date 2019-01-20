53e795e1afb0ca77adb28623644a928a35e3497e
VasilyAK <kaluginchernyhovsck@mail.ru>
2019-01-20 03:28:11 +0200

    Добавил второе задание

---
+++ lesson4/02.js
@@ -0,0 +1,45 @@
+// объект корзины состоит из имени товара, его колличества и цены
+function fnc_Product (name, count, price) {
+	this.name = name;
+	this.count = count;
+	this.price = price;
+}
+
+// массив содержит строки из корзины
+let arr_Busket = [];
+
+// заполняем массив произвольными значениями
+for (let i=0; i<10; i++) {
+	arr_Busket[i] = new fnc_Product('product'+i,i,i*500);
+}
+
+//функция вычисляет стоимость по конкретному товару. Считаем, что имя товара уникально
+function fn_CostByName(name) {
+	try {
+		if (arguments.length === 1){
+			if (typeof name === 'string'){
+				for (let i=0; i<arr_Busket.length; i++) {
+					if (arr_Busket[i].name === name) {
+						return arr_Busket[i].count*arr_Busket[i].price;
+					}
+				}
+				throw new Error(name + ' is not in Busket'); //сообщаем, что в корзине нет такого продукта
+			} else {
+				throw new Error('Function parameter must be \'string\'');
+			}
+		} else {
+			throw new Error('It must be only 1 parameter');
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+// функция считает общую стоимость корзины
+function fn_Cost() {
+	let cost = 0;
+	for (let i=0; i<arr_Busket.length; i++) {
+		cost += arr_Busket[i].count*arr_Busket[i].price;
+	}
+	return cost;
+}
