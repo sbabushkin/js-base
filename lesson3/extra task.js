9feeacc0495d9d1ad931fa04075a918abd821f1c
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-01-21 13:50:34 +0300

    fixed extra task

--- lesson3/extra task.js
+++ lesson3/extra task.js
@@ -1,10 +1,14 @@
-let massive = [0, 1, 2, 3, 4];
+let massive = [0, 1, 98, 3, 4, 44, 34, 67, 58];
 
 const reducer = (array) =>{
+	let maxNum = 0;
 	let lengthCount = array.reduce(function(accumulator, currentValue, index, array) {
-		console.log('tick');
-	  	return index;
+		// console.log(array[index]);
+		if(array[index] > maxNum){
+			maxNum = array[index];
+		}
+	  	return maxNum;
 	}, 10);
-	return lengthCount+1;
+	return lengthCount;
 }
 console.log(reducer(massive));
