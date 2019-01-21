09f17a75e723d57bdd20b3e5204f2967e269e006
VasilyAK <kaluginchernyhovsck@mail.ru>
2019-01-22 00:09:14 +0200

    Подправил скрипт во втором задании - удалил ненужное. Начал выполнение 3го задания.

--- lesson5/03/script.js
+++ lesson5/03/script.js
@@ -10,95 +10,14 @@ function fnc_Product (id, category, name, description, produccer, price, count, 
 	this.discount = discount; //скидка
 }
 
-// создадим корзину как объект
-let Busket = {
-	Count: function () {
-		try{
-			let count = 0;
-			for (let val in this) {
-				if (typeof this[val] === 'object'){
-					if (this[val].constructor.name === 'fnc_Product') {
-						count++;
-					}
-				}
-			}
-			return count;
-		} catch (e) {
-			console.log(e);
-		}
-	},
-
-	CostById: function (id) { //метод вычисляет стоимость по конкретному id товара, возвращает строку со стоимостью до 2х знаков полсе запятой
-		try {
-			if (arguments.length === 1){
-				if (typeof id === 'number'){
-					for (let val in this) {
-						if (typeof this[val] === 'object') {
-							if (this[val].hasOwnProperty('id') && this[val].id === id) {
-								if (this[val].hasOwnProperty('usercount') && !isNaN(this[val].usercount)) {
-									if (this[val].hasOwnProperty('price') && !isNaN(this[val].price)) {
-										if (this[val].hasOwnProperty('discount') && !isNaN(this[val].discount)) {
-											return (Math.round(this[val].usercount * this[val].price * (1 - this[val].discount) * 100) / 100).toFixed(2);
-										} else {
-											throw new Error('Cost of product' + id + ' cannot be calculated. Discount error.');
-										}
-									} else {
-										throw new Error('Cost of product' + id + ' cannot be calculated. Price error.');
-									}
-								} else {
-									throw new Error('Cost of product' + id + ' cannot be calculated. User count error.');
-								}
-							}
-						}
-					}
-					throw new Error(id + ' is not in Busket'); //сообщаем, что в корзине нет такого продукта
-				} else {
-					throw new Error('Function parameter must be \'number\'');
-				}
-			} else {
-				throw new Error('It must be only 1 parameter');
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	},
-
-	Cost: function () { // метод считает общую стоимость корзины, возвращает строку со стоимостью до 2х знаков полсе запятой
-		try{
-			let cost = 0;
-			for (let val in this) {
-				if (typeof this[val] === 'object'){
-					if (this[val].hasOwnProperty('usercount') && !isNaN(this[val].usercount)) {
-						if (this[val].hasOwnProperty('price') && !isNaN(this[val].price)) {
-							if (this[val].hasOwnProperty('discount') && !isNaN(this[val].discount)) {
-								cost += Math.round(this[val].usercount * this[val].price * (1-this[val].discount)*100)/100;
-							} else {
-								throw new Error('Cost cannot be calculated. Discount error.');
-							}
-						} else {
-							throw new Error('Cost cannot be calculated. Price error.');
-						}
-					} else {
-						throw new Error('Cost cannot be calculated. User count error.');
-					}
-				}
-			}
-			return cost.toFixed(2);
-		} catch (e) {
-			console.log(e);
-		}
-	}
-};
+let arr_Catalog = [];
 
 // создадим несколько позиций в корзине
-Busket['pos1'] = new fnc_Product(1, 'Жвачка', 'Бубльгум', 'Жутко вкусная жвачка', 'Bubl-production', 200, 800, 0.05);
-Busket.pos1.usercount = 5;
-Busket['pos2'] = new fnc_Product(2, 'Жвачка', 'Дирол', 'Мятная жвачка', 'Dirol-production', 400, 1500, 0.1);
-Busket.pos2.usercount = 3;
-Busket['pos3'] = new fnc_Product(3, 'Жвачка', 'Ментос', 'Мятная конфета', 'Mentos-production', 800, 400, 0.25);
-Busket.pos3.usercount = 3;
+arr_Catalog['pos1'] = new fnc_Product(1, 'Жвачка', 'Бубльгум', 'Жутко вкусная жвачка', 'Bubl-production', 200, 800, 0.05);
+arr_Catalog['pos2'] = new fnc_Product(2, 'Жвачка', 'Дирол', 'Мятная жвачка', 'Dirol-production', 400, 1500, 0.1);
+arr_Catalog['pos3'] = new fnc_Product(3, 'Жвачка', 'Ментос', 'Мятная конфета', 'Mentos-production', 800, 400, 0.25);
 
-function fn_BusketOpen() { //функция создает таблицу товаров корзины
+function fn_CatalogOpen() { //функция создает таблицу товаров корзины
 	try {
 		let div_busket = document.querySelector('#busket');
 		let boo_IsProduct = false; //проверяем, есть ли в корзине хотя бы один товар
