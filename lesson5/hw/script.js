98bd289c602d6f6c871d7a08bafdb1ee2ef36a1b
nanaxa <pavpildan@gmail.com>
2019-01-27 20:12:32 +0700

    homework 5 is done

---
+++ lesson5/hw/script.js
@@ -0,0 +1,58 @@
+//белые пешки
+document.getElementById("a2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("b2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("c2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("d2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("e2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("f2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("g2").style.backgroundImage = "url(img/peshkaWhite.png)";
+document.getElementById("h2").style.backgroundImage = "url(img/peshkaWhite.png)";
+
+//ладья белые 
+document.getElementById("a1").style.backgroundImage = "url(img/turaWhite.png)";
+document.getElementById("h1").style.backgroundImage = "url(img/turaWhite.png)";
+
+//коняга белая гнедая
+document.getElementById("b1").style.backgroundImage = "url(img/horseWhite.png)";
+document.getElementById("g1").style.backgroundImage = "url(img/horseWhite.png)";
+
+//белый офицер
+document.getElementById("c1").style.backgroundImage = "url(img/officerWhite.png)";
+document.getElementById("f1").style.backgroundImage = "url(img/officerWhite.png)";
+
+// белый ферзь
+document.getElementById("d1").style.backgroundImage = "url(img/ferzWhite.png)";
+
+//король белых
+document.getElementById("e1").style.backgroundImage = "url(img/kingWhite.png)";
+
+//черные пешки
+document.getElementById("a7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("b7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("c7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("d7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("e7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("f7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("g7").style.backgroundImage = "url(img/peshkaBlack.png)";
+document.getElementById("h7").style.backgroundImage = "url(img/peshkaBlack.png)";
+
+
+//ладья черная
+document.getElementById("a8").style.backgroundImage = "url(img/turaBlack.png)";
+document.getElementById("h8").style.backgroundImage = "url(img/turaBlack.png)";
+
+//черный конь
+document.getElementById("g8").style.backgroundImage = "url(img/horseBlack.png)"
+document.getElementById("b8").style.backgroundImage = "url(img/horseBlack.png)"
+
+//черный офицер
+document.getElementById("c8").style.backgroundImage = "url(img/officerBlack.png)";
+document.getElementById("f8").style.backgroundImage = "url(img/officerBlack.png)";
+
+//черная ферзя
+document.getElementById("d8").style.backgroundImage = "url(img/ferzBlack.png)";
+
+//Король черных
+document.getElementById("e8").style.backgroundImage = "url(img/kingBlack.png)";
+
+
