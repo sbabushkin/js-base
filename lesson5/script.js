e5c05a96f0f0f2ca83ff70e23fad91472948ab86
NastasyaLennon <ETGwerrgo13>
2019-01-27 19:44:59 +0300

    lesson5

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,15 +1,15 @@
 window.onload = function () {
 
-    let letter = ['a','b','c','d','e','f','g','h'];
+    let letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     let word = document.querySelector('.wordWhite');
-    for (wnum = 0; wnum < letter.length; wnum++){
+    for (wnum = 0; wnum < letter.length; wnum++) {
         let wordCell = document.createElement('div');
         word.appendChild(wordCell);
         wordCell.classList.add('wordCell');
         wordCell.innerHTML = letter[wnum];
     }
     let word2 = document.querySelector('.wordBlack');
-    for (wnum = 0; wnum < letter.length; wnum++){
+    for (wnum = 0; wnum < letter.length; wnum++) {
         let word2Cell = document.createElement('div');
         word2.appendChild(word2Cell);
         word2Cell.classList.add('wordCell');
@@ -18,14 +18,14 @@ window.onload = function () {
 
 
     let number = document.querySelector('.numberWhite');
-    for (num = 0; num < letter.length; num++){
+    for (num = 0; num < letter.length; num++) {
         let numCell = document.createElement('div');
         number.appendChild(numCell);
         numCell.classList.add('numCell');
         numCell.innerHTML = letter.length - num;
     }
     let number2 = document.querySelector('.numberBlack');
-    for (num = 0; num < letter.length; num++){
+    for (num = 0; num < letter.length; num++) {
         let num2Cell = document.createElement('div');
         number2.appendChild(num2Cell);
         num2Cell.classList.add('numCell');
@@ -45,11 +45,11 @@ window.onload = function () {
         i += ((i + 2) % 9) ? 1 : 2;
 
 
-        if (count < 16){
+        if (count < 16) {
             let figureBoardB = document.createElement('img');
             cell.appendChild(figureBoardB);
-            figureBoardB.setAttribute('src', 'img/chess/' + (count+1) + '.png');
-        }else if ( count > 47) {
+            figureBoardB.setAttribute('src', 'img/chess/' + (count + 1) + '.png');
+        } else if (count > 47) {
             let figureBoardW = document.createElement('img');
             cell.appendChild(figureBoardW);
             figureBoardW.setAttribute('src', 'img/chess/' + (count - 31) + '.png');
