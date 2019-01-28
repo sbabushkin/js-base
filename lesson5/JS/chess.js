4ce5ff2579bbf602f17c7e456f6df02a8afd5198
Crazymax21 <bpgachevm@gmail.com>
2019-01-28 19:41:19 +1000

    Чуть-чуть оптимизировал для того чтобы написать Refresh функцию.

--- lesson5/JS/chess.js
+++ lesson5/JS/chess.js
@@ -4,12 +4,11 @@ const pawn = {
     pic: 'pawn.png'
 , }
 const boardCell = function () {
-    this.height = 20;
-    this.width = 20;
+    this.id = '';
     this.cellColor = 'white'; //colors: white/black #000/#FFF
     this.cellType = 'none';
-    this.figureColor = 'white'; 
-    this.icon='';//black/white
+    this.figureColor = 'white';
+    this.icon = ''; //black/white
 }
 const chessBoard = [[new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell]
                     , [new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell, new boardCell]
@@ -22,78 +21,6 @@ const chessBoard = [[new boardCell, new boardCell, new boardCell, new boardCell,
 const cellName = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 function initBoard() {
-    for (let i = 0; i < chessBoard.length; i++) {
-        for (let j = 0; j < chessBoard[i].length; j++) {
-            chessBoard[i][j].cellType = '';
-            chessBoard[i][j].figureColor = '';
-            if (i == 1) {
-                chessBoard[i][j].cellType = 'PAWN';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9817';
-            }
-            if (i == 6) {
-                chessBoard[i][j].cellType = 'PAWN';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9823';
-            }
-            if (i == 0 && (j == 0 || j == 7)) {
-                chessBoard[i][j].cellType = 'ROOK';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9814';
-            }
-            if (i == 7 && (j == 0 || j == 7)) {
-                chessBoard[i][j].cellType = 'ROOK';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9820';
-            }
-            if (i == 0 && (j == 1 || j == 6)) {
-                chessBoard[i][j].cellType = 'HORSE';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9816';
-            }
-            if (i == 7 && (j == 1 || j == 6)) {
-                chessBoard[i][j].cellType = 'HORSE';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9822';
-            }
-            if (i == 0 && (j == 2 || j == 5)) {
-                chessBoard[i][j].cellType = 'BISHOP';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9815';
-            }
-            if (i == 7 && (j == 2 || j == 5)) {
-                chessBoard[i][j].cellType = 'BISHOP';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9821';
-            }
-            if (i == 0 && (j == 3)) {
-                chessBoard[i][j].cellType = 'QUEEN';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9813';
-            }
-            if (i == 7 && (j == 3)) {
-                chessBoard[i][j].cellType = 'QUEEN';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9819';
-            }
-            if (i == 0 && (j == 4)) {
-                chessBoard[i][j].cellType = 'KING';
-                chessBoard[i][j].figureColor = 'white';
-                chessBoard[i][j].icon='&#9812';
-            }
-            if (i == 7 && (j == 4)) {
-                chessBoard[i][j].cellType = 'KING';
-                chessBoard[i][j].figureColor = 'black';
-                chessBoard[i][j].icon='&#9818';
-            }
-        }
-    }
-    return;
-}
-
-function chess() {
-    let init = initBoard();
-    let parentDiv = document.getElementById("sChessBoard");
     let legendXDiv = document.getElementsByClassName('legendX');
     for (let k = 0; k < legendXDiv.length; k++) {
         for (let l = 0; l < chessBoard.length; l++) {
@@ -103,7 +30,7 @@ function chess() {
             legendXDiv[k].appendChild(legendNameX);
         }
     }
-    for (let i = chessBoard.length - 1; i >= 0; i--) {
+    for (let i = 0; i < chessBoard.length; i++) {
         //заполняем легендуY
         let legendYDiv = document.getElementsByClassName('legendY');
         if (legendYDiv != null) {
@@ -113,11 +40,95 @@ function chess() {
                 legendYDiv[k].appendChild(legendNameY);
             }
         }
+        for (let j = 0; j < chessBoard[i].length; j++) {
+            chessBoard[i][j].cellType = '';
+            chessBoard[i][j].figureColor = '';
+            if (i == 1) {
+                chessBoard[i][j].cellType = 'PAWN';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9817';
+            }
+            if (i == 6) {
+                chessBoard[i][j].cellType = 'PAWN';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9823';
+            }
+            if (i == 0 && (j == 0 || j == 7)) {
+                chessBoard[i][j].cellType = 'ROOK';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9814';
+            }
+            if (i == 7 && (j == 0 || j == 7)) {
+                chessBoard[i][j].cellType = 'ROOK';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9820';
+            }
+            if (i == 0 && (j == 1 || j == 6)) {
+                chessBoard[i][j].cellType = 'HORSE';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9816';
+            }
+            if (i == 7 && (j == 1 || j == 6)) {
+                chessBoard[i][j].cellType = 'HORSE';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9822';
+            }
+            if (i == 0 && (j == 2 || j == 5)) {
+                chessBoard[i][j].cellType = 'BISHOP';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9815';
+            }
+            if (i == 7 && (j == 2 || j == 5)) {
+                chessBoard[i][j].cellType = 'BISHOP';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9821';
+            }
+            if (i == 0 && (j == 3)) {
+                chessBoard[i][j].cellType = 'QUEEN';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9813';
+            }
+            if (i == 7 && (j == 3)) {
+                chessBoard[i][j].cellType = 'QUEEN';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9819';
+            }
+            if (i == 0 && (j == 4)) {
+                chessBoard[i][j].cellType = 'KING';
+                chessBoard[i][j].figureColor = 'white';
+                chessBoard[i][j].icon = '&#9812';
+            }
+            if (i == 7 && (j == 4)) {
+                chessBoard[i][j].cellType = 'KING';
+                chessBoard[i][j].figureColor = 'black';
+                chessBoard[i][j].icon = '&#9818';
+            }
+        }
+    }
+    return;
+}
+
+function change(id) {
+    for (let i = 0; i < chessBoard.length; i++) {
+        for (let j = 0; j < chessBoard[i].length; j++) {
+            if (chessBoard[i][j].id === id) {
+                let div = document.getElementById(id);
+                div.style = 'background-color: ' + 'gray';
+            }
+        }
+    }
+    return;
+}
+
+function chess() {
+    let parentDiv = document.getElementById("sChessBoard");
+    for (let i = chessBoard.length - 1; i >= 0; i--) {
         for (let j = 0; j <= chessBoard[i].length - 1; j++) {
             //Рисуем поле доски
             var div = document.createElement('div');
-            div.id = cellName[j]+''+Number(i+1);
+            div.id = cellName[j] + '' + Number(i + 1);
             div.className = 'cell' + String(i) + String(j) + ' cells';
+            div.setAttribute('onclick', 'change("' + String(cellName[j]) + String(Number(i + 1)) + '")');
             if ((i + 1 + j + 1) % 2 == 0) {
                 chessBoard[i][j].cellColor = 'darkkhaki';
             }
@@ -129,8 +140,10 @@ function chess() {
             if (parentDiv != null) {
                 parentDiv.appendChild(div);
             }
+            chessBoard[i][j].id = cellName[j] + '' + Number(i + 1);
             //      parentDiv.appendChild(div);
         }
     }
 }
+window.onload = initBoard();
 window.onload = chess();
