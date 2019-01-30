59de29570060286407d67a8388809a2035b52356
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-01-30 14:09:03 +0400

    upd modal window

--- lesson6/public/catalog.js
+++ lesson6/public/catalog.js
@@ -2,12 +2,13 @@ let catalogDb = {
 	// перечень товаров в категории Items
 	_items: {},
 	idStart: 9990,
+	placeHolderImg: './public/placeholder.jpg',
 	// ссылка на items:
 	get items(){
 		return this._items;
 	},
 	// функция добавления продукта
-	addItem(name, cost, details, img, img2, desc){
+	addItem(name, cost, details, img, img2, img3, desc){
 		let idNum = this.idStart;
 		let newItem = {
 			name,
@@ -16,6 +17,7 @@ let catalogDb = {
 			details,
 			img,
 			img2,
+			img3,
 			desc
 		};
 		this.items[idNum] = newItem;
@@ -160,7 +162,128 @@ let catalogDb = {
 			return;
 		}
 	},
+	// проверяем, есть ли картинка по ссылке
+	htmlPictureCheck(src, fallback){
+
+	    // let img = new Image();
+	    // img.onload = () => {return src}; 
+	    // img.onerror = () => {return fallback};
+	    // img.src = src;
+
+
+
+		// let image = new Image();
+		// console.log(src);
+		// image.src = src;
+		// console.log(image.width);
+		// if(image.width === 0){
+		// 	return fallback;
+		// } else {
+		// 	return src;
+		// }
+
+	},
+	// в текущей реализации, предполагается, что у меня только 3 картинки на объект, конечно, в реальном мире нужно использовать массив в котором не определенное количество элементов. Функции создания галереи не привязаны к количеству элементов, однако, функция отображения основного изображения, а так же изображения в каталоге нужно будет переделывать.
+	htmlGeneratePreviewGallery(id){
+		/*
+			<img class='galleryImg' src='' />
+			<img class='galleryImg' src='' />
+			<img class='galleryImg' src='' />
+		*/
+		const itemLink = this.items[id];
+		// checking if images exist, if not -> putting placeholder img
+		const img1src = itemLink.img;
+		const img2src = itemLink.img2;
+		const img3src = itemLink.img3;
+
+		const result = `<img class='galleryImg' src='${img1src}' alt='img' />
+						<img class='galleryImg' src='${img2src}' alt='img2' />
+						<img class='galleryImg' src='${img3src}' alt='img3' />`;
+		return result;
+
+	},
+	// добавляем подсветку элементу в модальном блоке
+	htmlAddPreviewPicturesBack(index){
+		const imagesPreviewLink = document.getElementsByClassName('galleryImg');
+		imagesPreviewLink[index].classList.add('chosenImg');
+
+	},
+	// убираем подсветку со всех элементов в модальном блоке
+	htmlCleanPreviewPicturesBack(){
+		const imagesPreviewLink = document.getElementsByClassName('galleryImg');
+		// убираем подсветку со всех элементов в блоке
+		for(let k=0; k<imagesPreviewLink.length;k++){
+			imagesPreviewLink[k].classList.remove('chosenImg');
+		};
+	},
+	htmlArrowFunction(event){
+		/*
+			NOTE that in order to compare images i use ALT attribute of IMG tag
+		*/
+		const tarLink = event.target; // link to target
+		// right or left arrow? 
+		const arrowDirection = tarLink.id;
+		// getting content of modal window
+		const modalImg = document.getElementById("modalImage");		
+		const imagesPreviewLink = document.getElementsByClassName('galleryImg');
+		// getting pic index in array and amount of pics in gallery
+		let picsIndexes = [];
+		let chosenIndex;
+
+		for(let i=0; i < imagesPreviewLink.length; i++){
+			// getting amount of pics
+			picsIndexes.push(i);
+			// getting current pic index using alt attribute
+			if(modalImg.alt === imagesPreviewLink[i].alt){
+				chosenIndex = i;
+			}
+		}
+
+		// replacing image in preview modal window:
+		let newChosenIndex;
+
+		if(arrowDirection === 'arrowRight'){
+			// if our image at the end of array
+			if(chosenIndex === picsIndexes.length-1){
+				modalImg.src = imagesPreviewLink[0].src;
+				modalImg.alt = imagesPreviewLink[0].alt;
+				newChosenIndex = 0;
+			} else {
+				modalImg.src = imagesPreviewLink[chosenIndex+1].src;
+				modalImg.alt = imagesPreviewLink[chosenIndex+1].alt;
+				newChosenIndex = chosenIndex+1;
+			}
+		} 
+
+		if(arrowDirection === 'arrowLeft') {
+			// if our image at the start of array
+			if(chosenIndex === 0){
+				modalImg.src = imagesPreviewLink[picsIndexes.length-1].src;
+				modalImg.alt = imagesPreviewLink[picsIndexes.length-1].alt;
+				newChosenIndex = picsIndexes.length-1;
+			} else {
+				modalImg.src = imagesPreviewLink[chosenIndex-1].src;
+				modalImg.alt = imagesPreviewLink[chosenIndex-1].alt;
+				newChosenIndex = chosenIndex-1;
+			}	
+		}
+
+		// now cleaning background selection
+
+		/*
+			since this is EVENT function i have to use GLOBAL link to object fucntions
+		*/
+
+		cdb.htmlCleanPreviewPicturesBack();
+
+		// adding bg to correct elemtn
+		cdb.htmlAddPreviewPicturesBack(newChosenIndex);
+	},
 	htmlPictureModule(id){
+		/*
+			NOTE that in order to compare images i use ALT attribute of IMG tag
+		*/
+
 		// Get the modal
 		const modal = document.getElementById('myModal');
 
@@ -171,7 +294,7 @@ let catalogDb = {
 			return;
 		} else {
 			// link to modal IMG container
-			let modalImg = document.getElementById("img01");
+			let modalImg = document.getElementById("modalImage");
 			// link to caption container
 			let captionText = document.getElementById("caption");
 
@@ -181,42 +304,46 @@ let catalogDb = {
 			captionText.innerHTML = this.items[id].details; // switch of alt
 
 			// Get the <span> element that closes the modal
-			let span = document.getElementsByClassName("close")[0];
+			let closeSpan = document.getElementsByClassName("close")[0];
 
 			// When the user clicks on <span> (x), close the modal
-			span.onclick = () => { 
+			closeSpan.onclick = () => { 
 			  modal.style.display = "none";
 			}
 		
 			/* putting previev gallery */
 				/* putting previev gallery */
 					/* putting previev gallery */
-			let previewLink = document.getElementById('preview');
-			let img2; 
+			const previewLink = document.getElementById('preview');
 
-			/* проверяем, есть ли элемнет на странице */
-			if(document.getElementById('img02')){
-				img2 = document.getElementById('img02');
-			/* если не нашли элемент на странице, то создаем */
-			} else {
-				img2 = this.htmlCreateForm('img', 'img02', '');
-				img2.setAttribute('src', this.items[id].img2);
-				this.htmlAppend(previewLink, img2);
+			this.htmlEdit(previewLink, this.htmlGeneratePreviewGallery(id));
+
+			// adding event listener on images click
+			const imagesPreviewLink = document.getElementsByClassName('galleryImg');
+			// вешаем событие на элементы для переключения:
+			for (let i=0; i<imagesPreviewLink.length;i++){
+				// вешаем событие на элементы для переключения:
+				imagesPreviewLink[i].addEventListener('click',(e)=>{
+					modalImg.src = imagesPreviewLink[i].src;
+					modalImg.alt = imagesPreviewLink[i].alt;
+					// убираем подсветку со всех элементов в блоке
+					this.htmlCleanPreviewPicturesBack();
+					// добавляем подсветку выделенному элементу
+					this.htmlAddPreviewPicturesBack(i);
+				});
+			// при открытии галерею, нужно подсветить первую картинку
+			this.htmlAddPreviewPicturesBack(0);
 			}
 
-			/* проверяем по значению alt */
-				/* меняем местами картинки по клику */
-			img2.onclick = () =>{
-				if(modalImg.alt === 'img'){
-					modalImg.src = this.items[id].img2; // switch of source
-					modalImg.alt = 'img2'; // switch of alt
-					img2.src = this.items[id].img; // switch of thumbnail
-				} else {
-					modalImg.src = this.items[id].img; // switch of source
-					modalImg.alt = 'img'; // switch of alt
-					img2.src = this.items[id].img2; // switch of thumbnail
-				} 
-			}
+			// ARROWS
+				// ARROWS
+					// ARROWS
+			document.getElementById('arrowRight').addEventListener('click', this.htmlArrowFunction);
+			document.getElementById('arrowLeft').addEventListener('click', this.htmlArrowFunction);
+
+			// func(arrow)
+
+
 			return;	
 		}
 	},
@@ -225,10 +352,10 @@ let catalogDb = {
 const cdb = catalogDb; // link to main obj
 
 /* Добавляем предметы в БД */
-cdb.addItem('Грелка', 50, 'KKG-001', './public/placeholder.png', './public/placeholder2.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
-cdb.addItem('Утюг', 100,'Samsung G433', 'public/placeholder.png', 'public/placeholder2.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
-cdb.addItem('Кофеварка', 50, 'Bork F123', 'public/placeholder.png', 'public/placeholder2.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
-cdb.addItem('Пылесос', 250, 'Dison J21', 'public/placeholder.png', 'public/placeholder2.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
+cdb.addItem('Грелка', 50, 'KKG-001', './public/1.jpg', './public/2.jpg', './public/3.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
+cdb.addItem('Утюг', 100,'Samsung G433', './public/1.jpg', './public/2.jpg', './public/3.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
+cdb.addItem('Кофеварка', 50, 'Bork F123', './public/1.jpg', './public/2.jpg', './public/3.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
+cdb.addItem('Пылесос', 250, 'Dison J21', './public/1.jpg', './public/2.jpg', './public/3.jpg', 'Описание продукта – это главное основание для покупки. От того, сможете ли вы убедить посетителя в потребности в вашем товаре зависит купит он его или нет. Страничка товара — это последний пункт в воронке продаж перед тем, как пользователь нажмет «оформить заказ».');
 console.log('Загруженный каталог товаров:');
 console.log(cdb.items);
 
