7e1b9f47174fd5f4576cde66a4f4372c67c7eef7
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-01-31 05:49:48 +0300

    Сделанно 1 задание 6 урока

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,21 +1,19 @@
 function myProcessor() {
   function showBig(event) {
-    const element = event.target;
-    const src = element.getAttribute('data-big-src');
-    const bigElement = document.getElementById('bigPicture');
-    bigElement.innerHTML = '<img height="200" src="./' + src + '"/>';
-    // element.removeEventListener('click', showBig);
+    const element = event.target; //img со всеми атрибутами
+    const src = element.getAttribute('data-big-src'); //src большой картинки
+    const notImg = "this.src = './img/big/standart.jpg'";//Если нет картинки то подставить этот src
+    const bigElement = document.getElementById('bigPicture'); //div id="bigPicture" с его содержимым
+    bigElement.innerHTML = '<img height="200" src="./' + src + '" onerror="' + notImg + '" />';
   }
-
-  const images = document.getElementsByTagName('img');
+  const images = document.getElementsByTagName('img'); //достали кортеж картинок
   for (let i = 0; i < images.length; i++) {
     const element = images[i];
-    element.addEventListener('click', showBig);
+    element.addEventListener('click', showBig); //добавили к каждой картинке событие 
   }
 }
 
 function myClick() {
-  console.log('click');
+  //console.log('click');
 }
-
 window.onload = myProcessor;
