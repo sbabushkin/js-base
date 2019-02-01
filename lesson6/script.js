badac7555a71a5f3efaa5e959a4ff1edd860accf
Crazymax21 <bpgachevm@gmail.com>
2019-02-01 15:02:54 +1000

    Сделал корзину.

--- lesson6/script.js
+++ lesson6/script.js
@@ -31,32 +31,69 @@ function myProcessor() {
         const images = document.getElementsByClassName('img-rolls');
         const src = images[imgIndex].getAttribute('data-big-src');
         let img = new Image();
-        img.src = './'+src;
-        img.onload = function() {
+        img.src = './' + src;
+        img.onload = function () {
             const bigElement = document.getElementById('bigPicture');
             bigElement.innerHTML = '<img height="200" src="./' + src + '"/>';
-            }
-        img.onerror = function(){
+        }
+        img.onerror = function () {
             const bigElement = document.getElementById('bigPicture');
             bigElement.innerHTML = '<img height="200" src="./img/error404.jpeg"/>';
         }
     }
 
-const images = document.getElementsByClassName('img-rolls');
-for (let i = 0; i < images.length; i++) {
-    const element = images[i];
-    element.addEventListener('click', showBig);
+    const images = document.getElementsByClassName('img-rolls');
+    for (let i = 0; i < images.length; i++) {
+        const element = images[i];
+        element.addEventListener('click', showBig);
+    }
+
+    const leftArrow = document.getElementById('left-arrow');
+    const rightArrow = document.getElementById('right-arrow');
+    leftArrow.addEventListener('click', prevImg);
+    rightArrow.addEventListener('click', nextImg);
+
 }
 
-const leftArrow = document.getElementById('left-arrow');
-const rightArrow = document.getElementById('right-arrow');
-leftArrow.addEventListener('click', prevImg);
-rightArrow.addEventListener('click', nextImg);
+/// Функции для магазина
+function shop() {
+    let thingsList = [];
+    //console.log('инициализация');
+    let costList = [['t1', 10000], ['t2', 5000], ['t3', 20000], ['t4', 50000], ['t5', 150000]]
 
+    const thingCollection = document.getElementsByClassName('things');
+    for (let i = 0; i < thingCollection.length; i++) {
+        thingCollection[i].addEventListener('click', addItemToRecycle);
+//        console.log('Добавили события');
+    }
+
+
+    function showRecycleCoast() {
+        let count = 0;
+        for (let i = 0; i < thingsList.length; i++) {
+            count += thingsList[i][1];
+        }
+        const cost = document.getElementById('reyclecount');
+        cost.innerHTML = 'В корзине '+ thingsList.length +' товаров на сумму: '+ count + ' р.';
+//        console.log(count);
+    }
+
+    function addItemToRecycle(event) {
+        const element = event.target;
+        thingsList.push(element.id);
+        console.log(thingsList);
+        thingsList[thingsList.length - 1] = [element.id];
+
+        for (let k = 0; k < costList.length; k++) {
+            if (costList[k][0] === element.id) {
+                thingsList[thingsList.length - 1].push(costList[k][1]);
+            }
+        }
+//        console.log(thingsList);
+        showRecycleCoast();
+
+    }
 }
-
-
-
 
 
 function myClick() {
@@ -64,3 +101,4 @@ function myClick() {
 }
 
 window.onload = myProcessor;
+window.onload = shop;
