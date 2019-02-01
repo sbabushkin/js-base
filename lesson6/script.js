10b46aaec3fac87d840fdc83ec02e89f1542c6de
nanaxa <pavpildan@gmail.com>
2019-02-01 22:26:36 +0700

    exersize 1 complete

--- lesson6/script.js
+++ lesson6/script.js
@@ -14,7 +14,8 @@ function myProcessor() {
   for (let i = 0; i < images.length; i++) {
     const element = images[i];
     element.addEventListener('click', showBig);
-  }
+        }
+
 }
 
 function myClick() {
@@ -22,9 +23,19 @@ function myClick() {
 }
     //проверка на существование по src
 
-   let images = document.getAttribute("data-big-src");
-var srcList = [];
-for(var i = 0; i < images.length; i++) {
-    srcList.push(images[i].src);
-} 
-console.log(srcList);
+window.onload = function sss() {
+    const images = document.getElementsByTagName('img');
+    let pictSrc = [];
+    for(let z = 0; z<images.length;z++) {
+        pictSrc.push(images[z])
+    }
+    
+    for(let i = 0; i < pictSrc.length; i++) {
+      if(pictSrc[i]) {
+          console.log("Картинка существует")
+      }
+      else {
+          console.log("Картинка несуществует")
+      }
+    }
+}
