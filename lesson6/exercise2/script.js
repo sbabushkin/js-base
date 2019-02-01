b108ec920cc6b027c7ef42a604455935fe07e267
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-02-01 20:06:46 +0300

    Улучшела 2 задание
    Добавила большие картинки при нажатии на маленькие

--- lesson6/exercise2/script.js
+++ lesson6/exercise2/script.js
@@ -3,41 +3,58 @@ const product = [{
   cost: 30,
   shortDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusamus iste veniam provident temporibus minus ea dolorem debitis voluptas officiis, fugiat non, nesciunt voluptatibus! Harum illo veniam optio aspernatur iusto.", //короткое аписание
   fullDescription: "https://ru.wikipedia.org/wiki/%D0%93%D1%83%D0%BF%D0%BF%D0%B8", //полное описание
-  imgURL: "gupimin.jpg"
+  imgURL: "gupimin.jpg",
+  imgURLbig: "gupibig.jpg"
     }, {
   name: "Петушки",
   cost: 700,
   shortDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusamus iste veniam provident temporibus minus ea dolorem debitis voluptas officiis, fugiat non, nesciunt voluptatibus! Harum illo veniam optio aspernatur iusto.", //короткое аписание
   fullDescription: "https://ru.wikipedia.org/wiki/%D0%91%D0%BE%D0%B9%D1%86%D0%BE%D0%B2%D0%B0%D1%8F_%D1%80%D1%8B%D0%B1%D0%BA%D0%B0", //полное описание
-  imgURL: "petushokmin.jpg"
+  imgURL: "petushokmin.jpg",
+  imgURLbig: "petushokbig.jpg"
     }, {
   name: "Золотая рыбка",
   cost: 450,
   shortDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusamus iste veniam provident temporibus minus ea dolorem debitis voluptas officiis, fugiat non, nesciunt voluptatibus! Harum illo veniam optio aspernatur iusto.", //короткое аписание
   fullDescription: "https://ru.wikipedia.org/wiki/%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F_%D1%80%D1%8B%D0%B1%D0%BA%D0%B0", //полное описание
-  imgURL: "goldfishmin.jpg"
+  imgURL: "goldfishmin.jpg",
+  imgURLbig: "goldfishbig.jpg"
     }, {
   name: "Афиохаракс Наттерера",
   cost: 300,
   shortDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusamus iste veniam provident temporibus minus ea dolorem debitis voluptas officiis, fugiat non, nesciunt voluptatibus! Harum illo veniam optio aspernatur iusto.", //короткое аписание
   fullDescription: "https://ru.wikipedia.org/wiki/%D0%A5%D0%B0%D1%80%D0%B0%D1%86%D0%B8%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5", //полное описание
-  imgURL: "aphyocharaxnattererimin.jpg"
+  imgURL: "aphyocharaxnattererimin.jpg",
+  imgURLbig: "aphyocharaxnattereribig.jpg"
     }, {
   name: "Афиосемион Огове",
   cost: 600,
   shortDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam accusamus iste veniam provident temporibus minus ea dolorem debitis voluptas officiis, fugiat non, nesciunt voluptatibus! Harum illo veniam optio aspernatur iusto.", //короткое аписание
   fullDescription: "https://ru.wikipedia.org/wiki/%D0%AE%D0%B6%D0%BD%D1%8B%D0%B9_%D0%B0%D1%84%D0%B8%D0%BE%D1%81%D0%B5%D0%BC%D0%B8%D0%BE%D0%BD", //полное описание
-  imgURL: "aphyosemionogoensemin.jpg"
+  imgURL: "aphyosemionogoensemin.jpg",
+  imgURLbig: "aphyosemionogoensebig.jpg"
     }];
+//картинки----------------------------------
+function showBig(event) {
+  //console.log(event.target);
+  const element = event.target; //img со всеми атрибутами
+  const src = element.getAttribute('data-big-src');
+  bigElement.innerHTML = '<span class="close"></span><img id="imgBig" src="./' + src + '" />';
+  bigElement.children[0].onclick = function () {
+    bigElement.innerHTML = '';
+  }
+}
 
 //Каталог-----------------------------------
-function creatingKatalog(srcImg, name, desc, namber, details, price) {
+function creatingKatalog(srcImg, srcImgBig, name, desc, namber, details, price) {
   var div = document.createElement('div');
   div.className = 'katalog';
   katalog.appendChild(div);
   var img = document.createElement('img');
   img.className = 'imgs-katalog';
   img.setAttribute('src', srcImg);
+  img.setAttribute('data-big-src', srcImgBig)
+  img.addEventListener('click', showBig);
   div.appendChild(img);
   var h3 = document.createElement('h3');
   h3.className = 'katalog-h';
@@ -59,7 +76,7 @@ function creatingKatalog(srcImg, name, desc, namber, details, price) {
   var aDesc = document.createElement('a');
   aDesc.className = 'products-btn';
   aDesc.setAttribute('href', details),
-  aDesc.innerHTML = 'подробнее';
+    aDesc.innerHTML = 'подробнее';
   div.appendChild(aDesc);
   var span = document.createElement('span');
   span.className = 'products-price';
@@ -68,7 +85,7 @@ function creatingKatalog(srcImg, name, desc, namber, details, price) {
 }
 //name, desc, namber, details, price
 for (let i = 0; i < product.length; i++) {
-  creatingKatalog('img/' + product[i].imgURL, product[i].name, product[i].shortDescription, i, product[i].fullDescription, product[i].cost + ' RUB');
+  creatingKatalog('img/' + product[i].imgURL, 'img/' + product[i].imgURLbig, product[i].name, product[i].shortDescription, i, product[i].fullDescription, product[i].cost + ' RUB');
 }
 // корзина-----------------------------
 function creatingBasket(name, desc, price) {
--- lesson6/script.js
+++ lesson6/script.js
@@ -1,58 +1,52 @@
 function myProcessor() {
+  const bigElement = document.getElementById('bigPicture');
+
   function showBig(event) {
     const element = event.target; //img со всеми атрибутами
     const src = element.getAttribute('data-big-src'); //src большой картинки
     const notImg = "this.src = './img/big/standart.jpg'"; //Если нет картинки то подставить этот src
-    const bigElement = document.getElementById('bigPicture'); //div id="bigPicture" с его содержимым
-    bigElement.innerHTML = '<img height="200" id="imgBig" src="./' + src + '" onerror="' + notImg + '" />';
+    bigElement.innerHTML = '<img height="200" id="imgBig" data-src="' + src + '" src="./' + src + '" onerror="' + notImg + '" />';
   }
   const images = document.getElementsByTagName('img'); //достали кортеж картинок
   for (let i = 0; i < images.length; i++) {
     const element = images[i];
     element.addEventListener('click', showBig); //добавили к каждой картинке событие 
   }
-//Дастаем сылки
-const controlLeft = document.getElementById("left");
-const controlRight = document.getElementById("right");
-const bigElement = document.getElementById('bigPicture');
-  
-controlLeft.onclick = function () {
-  if (bigElement.children.length) {
-    switch (imgBig.src.slice(-6)){
-      case '1.jpeg': 
-        imgBig.src = './img/big/3.jpg';
-        break;
+
+  left.onclick = function () {
+    if (bigElement.children.length) {
+      switch (imgBig.src.slice(-6)) {
+        case '1.jpeg':
+          imgBig.src = './img/big/3.jpg';
+          break;
         case '2.jpeg':
-        imgBig.src = './img/big/1.jpeg';
-        break;
-        case '/3.jpg': 
-        imgBig.src = './img/big/2.jpeg';
-        break;
+          imgBig.src = './img/big/1.jpeg';
+          break;
+        case '/3.jpg':
+          imgBig.src = './img/big/2.jpeg';
+          break;
+      }
+    } else {
+      console.log('нет картинки');
     }
-  } else {
-    console.log('нет картинки');
-  }
-};
-controlRight.onclick = function () {
-  if (bigElement.children.length) {
-    switch (imgBig.src.slice(-6)){
-      case '1.jpeg': 
-        imgBig.src = './img/big/2.jpeg';
-        break;
+  };
+  right.onclick = function () {
+    if (bigElement.children.length) {
+      switch (imgBig.src.slice(-6)) {
+        case '1.jpeg':
+          imgBig.src = './img/big/2.jpeg';
+          break;
         case '2.jpeg':
-        imgBig.src = './img/big/3.jpg';
-        break;
-        case '/3.jpg': 
-        imgBig.src = './img/big/1.jpeg';
-        break;
+          imgBig.src = './img/big/3.jpg';
+          break;
+        case '/3.jpg':
+          imgBig.src = './img/big/1.jpeg';
+          break;
+      }
+    } else {
+      console.log('нет картинки');
     }
-    
-  } else {
-    console.log('нет картинки');
   }
 }
-}
-function myClick() {
-  //console.log('click');
-}
+
 window.onload = myProcessor;
