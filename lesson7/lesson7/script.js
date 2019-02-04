f60bef77da6be09eaee9cf30976ec55a4d921f47
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-02-04 17:57:37 +0300

    Добавила уровни к игре

--- lesson7/lesson7/script.js
+++ lesson7/lesson7/script.js
@@ -2,7 +2,7 @@
 let interval; // canvas
 let canv; // 2d context
 let ctx; // player X position
-let px;// player Y position
+let px; // player Y position
 let py;
 
 const SNAKE_COLOR_ODD = 'blue';
@@ -11,22 +11,22 @@ const APPLE_COLOR = 'green';
 
 // game started && first key pressed (initialization states)
 const gs = false;
-let fkp = false;// snake movement speed
+let fkp = false; // snake movement speed
 const baseSpeed = 3;
-let speed = baseSpeed;// velocity (x & y)
+let speed = baseSpeed; // velocity (x & y)
 let xv = 0;
 let yv = 0;
 
 // player size
 const pw = 20;
-const ph = 20;// apple size
+const ph = 20; // apple size
 const aw = 20;
-const ah = 20;// apples list
-const apples = [];// список элементов яблок
-const obstacles = [];//список элементов преград
-const tailPieces = [];// tail size (1 for 10)
-let tailSize = 20;// self eating protection for head zone (aka safeZone)
-const tailSizeMin = 20;// is key in cooldown mode
+const ah = 20; // apples list
+const apples = []; // список элементов яблок
+const obstacles = []; //список элементов преград
+const tailPieces = []; // tail size (1 for 10)
+let tailSize = 20; // self eating protection for head zone (aka safeZone)
+const tailSizeMin = 20; // is key in cooldown mode
 const cooldown = false;
 const score = 0; // current score
 let appleEat = 0; //Количества съеденных яблок
@@ -35,18 +35,18 @@ let live = 0;
 // функция рисования яблока
 function spawnApple() {
   const newApple = {
-    x: ~~(Math.random() * canv.width - 2 * aw) + aw,
-    y: ~~(Math.random() * canv.height - 2 * ah) + ah,
+    x: ~~(Math.random() * ((canv.width - 40) - 2 * aw)) + aw * 2,
+    y: ~~(Math.random() * ((canv.height - 40) - 2 * ah)) + ah * 2,
     color: APPLE_COLOR,
   };
 
   // проверка что яблоко не попало на хвост
   for (let i = 0; i < tailSize.length; i++) {
     if (
-      newApple.x < (tailPieces[i].x + pw) &&
-      newApple.x + aw > tailPieces[i].x &&
-      newApple.y < (tailPieces[i].y + ph) &&
-      newApple.y + ah > tailPieces[i].y
+      (newApple.x < (tailPieces[i].x + pw) &&
+        newApple.x + aw > tailPieces[i].x &&
+        newApple.y < (tailPieces[i].y + ph) &&
+        newApple.y + ah > tailPieces[i].y)
     ) {
       spawnApple();
       return;
@@ -70,7 +70,6 @@ function loop() {
   // logic
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canv.width, canv.height);
-
   // force speed
   px += xv;
   py += yv;
@@ -141,27 +140,121 @@ function loop() {
       spawnApple(); // spawn another apple(-s)
       spawnObstacles();
       obstacles.splice(a, 1);
-      appleEat++;//подщет очков
+      appleEat++; //подщет очков
       applEat.innerHTML = 'Счет: ' + appleEat;
       break;
     }
   }
+  //уровни сложности
+  if (appleEat >= 5 && appleEat < 10) {
+    level1();
+  } else if (appleEat >= 10 && appleEat < 15) {
+    level2();
+  } else if (appleEat >= 15 && appleEat < 20) {
+    level3();
+  } else if (appleEat >= 20 && appleEat < 30) {
+    level4();
+  } else if (appleEat >= 30) {
+    applEat.innerHTML = 'Поздравляю вы выиграли!';
+  }
+}
 
-  // рисовать преграды
+function level1() {
+  //рисовать препятствия
   for (let a = 0; a < obstacles.length; a++) {
     ctx.fillStyle = 'red';
     ctx.fillRect(obstacles[a].x, obstacles[a].y, obstacles[a].plesx, obstacles[a].plesy);
+    lev2.className = 'play';
   }
 
   // проверить на столкновение головы змеи с Obstacles
   for (let a = 0; a < obstacles.length; a++) {
     if (
-      px < (obstacles[a].x + obstacles[a].plesx) &&//право
-      px + 20 > obstacles[a].x &&//лево
-      py < (obstacles[a].y + obstacles[a].plesy) &&//низ
-      py + 20 > obstacles[a].y//верх
+      px < (obstacles[a].x + obstacles[a].plesx) && //право
+      px + 20 > obstacles[a].x && //лево
+      py < (obstacles[a].y + obstacles[a].plesy) && //низ
+      py + 20 > obstacles[a].y //верх
     ) {
       applEat.innerHTML = 'Игра закончена! Вы набрали ' + appleEat + ' очков.';
+      stop();
+      break;
+    }
+  }
+}
+
+function level2() {
+  //рисовать препятствия
+  for (let a = 0; a < obstacles.length; a++) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 600, 20);
+    ctx.fillRect(0, 380, 600, 400);
+    lev3.className = 'play';
+  }
+
+  // проверить на столкновение головы змеи с Obstacles
+  for (let a = 0; a < obstacles.length; a++) {
+    if (
+      (px < (0 + 600) && px + 20 > 0 && py < (0 + 18) && py + 20 > 0) ||
+      (px < (0 + 600) && px + 20 > 0 && py < (385 + 400) && py + 20 > 385)
+    ) {
+      applEat.innerHTML = 'Игра закончена! Вы набрали ' + appleEat + ' очков.';
+      stop();
+      break;
+    }
+  }
+}
+
+function level3() {
+  //рисовать препятствия
+  for (let a = 0; a < obstacles.length; a++) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 600, 20);
+    ctx.fillRect(0, 380, 600, 400);
+    ctx.fillRect(0, 0, 20, 400);
+    ctx.fillRect(580, 0, 600, 400);
+    lev4.className = 'play';
+  }
+
+  // проверить на столкновение головы змеи с Obstacles
+  for (let a = 0; a < obstacles.length; a++) {
+    if (
+      (px < (0 + 600) && px + 20 > 0 && py < (0 + 18) && py + 20 > 0) ||
+      (px < (0 + 18) && px + 20 > 0 && py < (0 + 400) && py + 20 > 0) ||
+      (px < (0 + 600) && px + 20 > 0 && py < (382 + 400) && py + 20 > 382) ||
+      (px < (582 + 600) && px + 20 > 582 && py < (0 + 400) && py + 20 > 0)
+    ) {
+      applEat.innerHTML = 'Игра закончена! Вы набрали ' + appleEat + ' очков.';
+      stop();
+      break;
+    }
+  }
+}
+
+function level4() {
+  //рисовать препятствия
+  for (let a = 0; a < obstacles.length; a++) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 600, 20);
+    ctx.fillRect(0, 380, 600, 400);
+    ctx.fillRect(0, 0, 20, 400);
+    ctx.fillRect(580, 0, 600, 400);
+    ctx.fillRect(obstacles[a].x, obstacles[a].y, obstacles[a].plesx, obstacles[a].plesy);
+    lev5.className = 'play';
+  }
+
+  // проверить на столкновение головы змеи с Obstacles
+  for (let a = 0; a < obstacles.length; a++) {
+    if (
+      (px < (0 + 600) && px + 20 > 0 && py < (0 + 18) && py + 20 > 0) ||
+      (px < (0 + 18) && px + 20 > 0 && py < (0 + 400) && py + 20 > 0) ||
+      (px < (0 + 600) && px + 20 > 0 && py < (382 + 400) && py + 20 > 382) ||
+      (px < (582 + 600) && px + 20 > 582 && py < (0 + 400) && py + 20 > 0) ||
+      (px < (obstacles[a].x + obstacles[a].plesx) &&
+        px + 20 > obstacles[a].x &&
+        py < (obstacles[a].y + obstacles[a].plesy) &&
+        py + 20 > obstacles[a].y)
+    ) {
+      applEat.innerHTML = 'Поздравляю вы добрались до 5 уровня! Вы набрали ' + appleEat + ' очков.';
       stop();
       break;
     }
