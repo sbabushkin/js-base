30acfaf2cfe5254a31e3403dc7ca539c2aa13549
KaterinaLove <KaterinaLove@users.noreply.github.com>
2019-02-06 10:19:46 +0300

    Последнее задание

--- lesson8/script.js
+++ lesson8/script.js
@@ -324,3 +324,33 @@ function stop() {
   clearInterval(interval);
 }
 window.onload = init;
+/*
+if (!("a" in window)) {
+    var a = 1;
+}
+alert(a); //1
+
+var b = function a(x) {
+    x && a(--x);
+};
+alert(a); //ошибка
+
+function a(x) {
+    return x * 2;
+}
+var a;
+alert(a);   //function a(x) {
+            //  return x * 2;
+            //}
+
+function b(x, y, a) {
+    arguments[2] = 10;
+    alert(a);
+}
+b(1, 2, 3); //10
+
+function a() {
+    alert(this);
+}
+a.call(null); //[object Window]
+*/
